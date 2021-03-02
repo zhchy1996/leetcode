4a3725e1a2e8931f28b19e2f4922d7730b9f716a
chengyuzhang <chengyuzhang@yangqianguan.com>
2021-03-02 20:03:57 +0800

    5 done

--- 5.最长回文子串.js
+++ 5.最长回文子串.js
@@ -81,8 +81,68 @@
 // };
 
 // 动态规划写法
+/**
+ * 用s[i][j]表示从第i个字符到第j个字符的字符串，如果
+ */
 var longestPalindrome = function(s) {
-    
+    const len = s.length;
+    // 构造 len * len 矩阵，用来记录回文串情况
+    const matrix = new Array(len);
+    let res = '';
+    for (let i = 0; i < len; i ++) {
+        matrix[i] = new Array(len);
+    }
+
+    for (let l = 0; l < len; l++) {
+        for(let i = 0; i + l < len; i ++) {
+            const j = i + l;
+            if (l === 0) {
+                matrix[i][j] = true;
+            } else if (l === 1) {
+                matrix[i][j] = s[i] === s[j];
+            } else {
+                // 字符串是回文的条件：s[i] === s[j] 并且起子串也是回文
+                // 由于需要依赖子串，所以我们可以从子串开始判断，所以最外层循环是子串长度，从小到大即可
+                matrix[i][j] = s[i] === s[j] && matrix[i + 1][j - 1];
+            }
+
+            if (matrix[i][j] && l + 1 > res.length) {
+                res = s.substring(i, j + 1);
+            }
+        }
+    }
+
+    return res;
+};
+
+// 中心扩展法
+var longestPalindrome = function(s) {
+    let start = 0,
+        end = 0;
+    const len = s.length;
+
+    if (s.length === 0) return '';
+
+    for (let k = 0; k < len; k ++) {
+        const len1 = expand(k, k);
+        const len2 = expand(k, k + 1);
+        const maxLen = Math.max(len1, len2);
+        if (maxLen > end - start + 1) {
+            start = k - Math.floor((maxLen - 1) / 2);
+            end = k + Math.floor(maxLen / 2);
+        }
+        
+    }
+
+    function expand(i, j) {
+        while(i > -1 && j < len && s[i] == s[j]) {
+            i--;
+            j++;
+        }
+        return j - i - 1;
+    }
+
+    return s.substring(start, end + 1);
 };
 
 // @lc code=end
